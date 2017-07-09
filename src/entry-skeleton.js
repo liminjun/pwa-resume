4980084222413e8412fa31a1167c802d95343c61
lee <v-leeli@hotmail.com>
2017-07-09 21:23:55 +0800

    feat:init the codes.

---
+++ src/entry-skeleton.js
@@ -0,0 +1,14 @@
+/**
+ * @file 页面骨架
+ * @author lee(v-leeli@hotmail.com)
+ */
+
+import Vue from 'vue';
+import Skeleton from '@/pages/Skeleton';
+
+export default new Vue({
+    components: {
+        Skeleton
+    },
+    template: '<skeleton />'
+});
